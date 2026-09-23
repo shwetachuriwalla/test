fef044ccc6b1d503cbf05b0870c5208ec636279f
Shweta <sharmashweta86@gmail.com>
2026-09-23 17:51:44 +0800

    commit helloworld shweta

---
+++ helloworld_shweta/src/test/java/helloworld_shweta/TestHelloWorld.java
@@ -0,0 +1,15 @@
+package helloworld_shweta;
+
+import org.testng.annotations.Test;
+
+public class TestHelloWorld {
+
+	@Test
+	public  void testHelloWorld() {
+		
+		System.out.println("..Hello World...!");
+	
+
+	}
+
+}
---
+++ test/src/test/ExceptionHandlingDemo.java
@@ -0,0 +1,26 @@
+package test;
+
+public class ExceptionHandlingDemo {
+
+
+	public static void main(String[] args) {
+	  try {
+		demo();
+	  } catch (Exception e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	  }	
+
+	}
+	public static void demo() throws Exception {
+		
+			
+			System.out.println("Hello World...!");
+			throw new ArithmeticException("not valid operation");
+			//int i = 1/0;
+			//System.out.println("Completed");
+			
+		
+	}
+
+}
